e370d5d435c06d2561166b58e1d133023f37579c
frevert.amelia@gmail.com <frevert.amelia@gmail.com>
2015-10-28 11:07:31 -0500

    first addition of angular

--- server/public/assets/scripts/optionsController.js
+++ server/public/assets/scripts/optionsController.js
@@ -1,3 +1,39 @@
+var app = angular.module('whiteflag', []);
+
 app.controller('optionsController', ['$scope', function($scope){
+
+    $scope.divSlow = true;
+    $scope.divConnect = true;
+    $scope.divRepeat = true;
+    $scope.divLost = true;
+    $scope.divBreak = true;
+    $scope.divGood = true;
+
+    $scope.opacitySlow = function(){
+        $scope.divSlow = false;
+
+    };
+
+    $scope.opacityConnect = function(){
+        $scope.divConnect = false;
+    };
+
+    $scope.opacityRepeat = function(){
+        $scope.divRepeat = false;
+    };
+
+    $scope.opacityLost = function(){
+        $scope.divLost = false;
+    };
+
+    $scope.opacityBreak = function(){
+        $scope.divBreak = false;
+    };
+
+    $scope.opacityGood = function(){
+        $scope.divGood = false;
+    };
+
+    $scope.classSlow =
 
 }]);
